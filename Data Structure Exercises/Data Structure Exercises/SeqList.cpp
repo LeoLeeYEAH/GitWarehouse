c1de1a9be53e7e04efb0d0100cf9e5341ef054d6
LeeLeo <liruicheng1992@sina.com>
2018-08-03 12:07:24 +0800

    delete useless files

---
+++ Data Structure Exercises/Data Structure Exercises/SeqList.cpp
@@ -0,0 +1,9 @@
+//
+//  SeqList.cpp
+//  Data Structure Exercises
+//
+//  Created by LeeLeo on 18/7/25.
+//  Copyright © 2018年 LeeLeo. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Data Structure Exercises/Data Structure Exercises/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structure Exercises
+//
+//  Created by LeeLeo on 18/7/25.
+//  Copyright © 2018年 LeeLeo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
