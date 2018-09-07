f44e00bcb6c0f76c66a34bcd5f4a81c0c98d45fe
LeeLeo <liruicheng1992@sina.com>
2018-09-07 11:28:12 +0800

    delete some files

--- Data Structure Exercises/Data Structure Exercises/Chapter2Tests.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Chapter2Tests.cpp
-//  Data Structure Exercises
-//
-//  Created by LeeLeo on 18/7/25.
-//  Copyright © 2018年 LeeLeo. All rights reserved.
-//
-
-#include <stdio.h>
--- Data Structure Exercises/Data Structure Exercises/SeqList.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SeqList.cpp
-//  Data Structure Exercises
-//
-//  Created by LeeLeo on 18/7/25.
-//  Copyright © 2018年 LeeLeo. All rights reserved.
-//
-
-#include <stdio.h>
